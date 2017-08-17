9b7316cafbecad7bf3c0971feca131df95f48ce1
illuggah <illuggah@mail.ru>
2017-08-17 07:57:13 +0700

    Update php-lesson3.php

--- php-lesson3.php
+++ php-lesson3.php
@@ -20,13 +20,13 @@
 		$wrdcnt = str_word_count($vs);
 		if ($wrdcnt == 2) {
 			$double = str_word_count($vs, 1);
-			$array2[first][] = $double[0];
-			$array2[second][] = $double[1];
+			$array2['first'][] = $double[0];
+			$array2['second'][] = $double[1];
 		}
 	}
 
-	shuffle($array2[first]);
-	shuffle($array2[second]);
+	shuffle($array2['first']);
+	shuffle($array2['second']);
 	
 ?>
 <!DOCTYPE html>
@@ -34,22 +34,21 @@
 <head>
 	<meta charset="UTF-8">
 	<title>php-lesson3</title>
-
 </head>
 <body>
 	<?php
 
-		echo $array2[first][0] . ' ' . $array2[second][0] . ',<br>';
-		echo $array2[first][1] . ' ' . $array2[second][1] . ',<br>';
-		echo $array2[first][2] . ' ' . $array2[second][2] . ',<br>';
-		echo $array2[first][3] . ' ' . $array2[second][3] . ',<br>';
-		echo $array2[first][4] . ' ' . $array2[second][4] . ',<br>';
-		echo $array2[first][5] . ' ' . $array2[second][5] . ',<br>';
-		echo $array2[first][6] . ' ' . $array2[second][6] . ',<br>';
-		echo $array2[first][7] . ' ' . $array2[second][7] . ',<br>';
-		echo $array2[first][8] . ' ' . $array2[second][8] . ',<br>';
-		echo $array2[first][9] . ' ' . $array2[second][9] . ',<br>';
-		echo $array2[first][10] . ' ' . $array2[second][10] . '<br>';
+		echo $array2['first'][0] . ' ' . $array2['second'][0] . ',<br>';
+		echo $array2['first'][1] . ' ' . $array2['second'][1] . ',<br>';
+		echo $array2['first'][2] . ' ' . $array2['second'][2] . ',<br>';
+		echo $array2['first'][3] . ' ' . $array2['second'][3] . ',<br>';
+		echo $array2['first'][4] . ' ' . $array2['second'][4] . ',<br>';
+		echo $array2['first'][5] . ' ' . $array2['second'][5] . ',<br>';
+		echo $array2['first'][6] . ' ' . $array2['second'][6] . ',<br>';
+		echo $array2['first'][7] . ' ' . $array2['second'][7] . ',<br>';
+		echo $array2['first'][8] . ' ' . $array2['second'][8] . ',<br>';
+		echo $array2['first'][9] . ' ' . $array2['second'][9] . ',<br>';
+		echo $array2['first'][10] . ' ' . $array2['second'][10] . '<br>';
 
 	?>
 </body>
